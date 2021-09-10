522fceda20fc5b2b3e9af3147bc311bfc21e614b
ckconur <cekiconurr@gmail.com>
2021-09-10 18:04:09 +0300

    Şifremi unuttum modal oluşturuldu, animasyonu eklendi. Modal açma kapatma JS kodları yazıldı.

--- js/app.js
+++ js/app.js
@@ -1,15 +1,37 @@
 let slideIndex = 0;
 slider();
-
+window.addEventListener('click',outsideClick);
 
 function slider() {
     var i;
     var x = document.getElementsByClassName("mySlides");
-    for (i = 0; i < x.length; i++) {
-        x[i].style.display = "none";
+    
+    if (x.length > 0) {
+        for (i = 0; i < x.length; i++) {
+            x[i].style.display = "none";
+        }
+        slideIndex++;
+        if (slideIndex > x.length) { slideIndex = 1 }
+        x[slideIndex - 1].style.display = "block";
+        setTimeout(slider, 4000);
     }
-    slideIndex++;
-    if (slideIndex > x.length) { slideIndex = 1 }
-    x[slideIndex - 1].style.display = "block";
-    setTimeout(slider, 4000);
 }
+
+function openModal(modalId){
+    let modal = document.getElementById(modalId);
+    modal.style.display = 'block';
+};
+
+function closeModal(modalId){
+    let modal = document.getElementById(modalId);
+    modal.style.display = 'none';
+};
+
+function outsideClick(ev){
+    let modal = document.getElementsByClassName('modal')[0];
+    let modalId = modal.id;
+    if (ev.target.id == modalId) {
+        modal.style.display = 'none';
+    }
+}
+
