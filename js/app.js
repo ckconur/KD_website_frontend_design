effbed327ee53898c7deef31b3340bcc76ce62fd
ckconur <cekiconurr@gmail.com>
2021-09-10 12:27:37 +0300

    first commit

---
+++ js/app.js
@@ -0,0 +1,15 @@
+let slideIndex = 0;
+slider();
+
+
+function slider() {
+    var i;
+    var x = document.getElementsByClassName("mySlides");
+    for (i = 0; i < x.length; i++) {
+        x[i].style.display = "none";
+    }
+    slideIndex++;
+    if (slideIndex > x.length) { slideIndex = 1 }
+    x[slideIndex - 1].style.display = "block";
+    setTimeout(slider, 4000);
+}
