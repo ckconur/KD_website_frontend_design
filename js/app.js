e64e2beca12a4890643ee7e4624ef2d0b5ba5542
ckconur <cekiconurr@gmail.com>
2021-09-21 22:19:49 +0300

    pie chart eklendi

--- js/app.js
+++ js/app.js
@@ -1,11 +1,11 @@
 let slideIndex = 0;
 slider();
-window.addEventListener('click',outsideClick);
+window.addEventListener('click', outsideClick);
 
 function slider() {
     var i;
     var x = document.getElementsByClassName("mySlides");
-    
+
     if (x.length > 0) {
         for (i = 0; i < x.length; i++) {
             x[i].style.display = "none";
@@ -17,17 +17,17 @@ function slider() {
     }
 }
 
-function openModal(modalId){
+function openModal(modalId) {
     let modal = document.getElementById(modalId);
     modal.style.display = 'block';
 };
 
-function closeModal(modalId){
+function closeModal(modalId) {
     let modal = document.getElementById(modalId);
     modal.style.display = 'none';
 };
 
-function outsideClick(ev){
+function outsideClick(ev) {
     let modal = document.getElementsByClassName('modal')[0];
     if (modal !== undefined) {
         let modalId = modal.id;
@@ -35,5 +35,53 @@ function outsideClick(ev){
             modal.style.display = 'none';
         }
     }
+}
+
+function makeChart() {
+    let myChart = document.getElementById('my-chart').getContext('2d');
+
+    let massPopChart = new Chart(myChart, {
+        type: 'doughnut',
+        data: {
+            labels: ['Boston', 'Worchester', 'Springfield', 'Cambridge', 'New Bedford'],
+            datasets: [{
+                label: 'Population',
+                data: [
+                    617000,
+                    181000,
+                    153000,
+                    106000,
+                    105000,
+                    950000
+                ],
+                backgroundColor:[
+                    '#317F6E',
+                    '#CCAA43',
+                    '#24549E',
+                    '#4BAF38',
+                    '#DB0E85',
+                    '#EC561A',
+                    '#967CF0',
+                    '#5B0C49',
+                    '#F5B8FC',
+                    '#A210EB',
+                    '#0C9B12',
+                    '#BD232F',
+                    '#A54284',
+                    '#582ADF',
+                    '#D7BF29',
+                    '#A436DE',
+                    '#A30C91',
+                    '#52036A',
+                    '#074A58',
+                    '#23ACF9',
+                ],
+                hoverOfset: 250,
+            }]
+        },
+        options: {
+
+        }
+    })
 }
 
